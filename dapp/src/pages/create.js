a079c55d0b0540570669a3c1bb9e091f969160b1
AmandaSacchi <amandasacchi@hotmail.com>
2024-03-22 18:32:15 -0300

    Adição de header nas paginas

--- dapp/src/pages/create.js
+++ dapp/src/pages/create.js
@@ -2,7 +2,9 @@ import Head from "next/head";
 import Link from "next/link";
 import Footer from "@/components/Footer";
 import { useState } from "react";
-import { addOng } from "../services/Web3Service"; // Asumindo que você tem uma função addOng em seus serviços
+import { addOng } from "../services/Web3Service";
+import React from 'react';
+import styles from '@/styles/teste.module.css';
 
 export default function CreateOng() {
     const [ong, setOng] = useState({ nome: '', causa: '', cnpj: '' });
@@ -25,42 +27,56 @@ export default function CreateOng() {
     return (
         <>
             <Head>
-                <title>GLUPS | Cadastrar ONG</title>
-                <meta charSet='utf-8' />
-                <meta name="description" content="Cadastre sua ONG na plataforma GLUPS e ajude a fazer a diferença." />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <title>Glups - Home</title>
+                <meta name="description" content="Glups" />
             </Head>
-            <div className="container">
-                <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Cadastrar ONG</h1>
-                <p>Preencha os campos para incluir sua ONG na plataforma.</p>
-                <hr className="mb-4" />
-                <div className="col-6">
-                    <div className="form-floating mb-3">
-                        <input type="text" id="nome" name="nome" className="form-control" value={ong.nome} onChange={onInputChange} />
-                        <label htmlFor="nome">Nome da Organização:</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="text" id="causa" name="causa" className="form-control" value={ong.causa} onChange={onInputChange} />
-                        <label htmlFor="causa">Causa:</label>
-                    </div>
-                    <div className="form-floating mb-3">
-                        <input type="text" id="cnpj" name="cnpj" className="form-control" value={ong.cnpj} onChange={onInputChange} />
-                        <label htmlFor="cnpj">CNPJ:</label>
+
+            <main className={styles.main}>
+                <div className={styles.sobreposicao}>
+
+                    <header className={styles.header}>
+                        <nav className={styles.navbar}>
+                            <div className={styles.logo}>Glups</div>
+                            <div className={styles.navItems}>
+                                <a href="/" className={styles.navLink}>Home</a>
+                                <a href="/about" className={styles.navLink}>Sobre</a>
+                                <a href="/create" className={styles.navLink}>Cadatrar ONG</a>
+                                <a href="/donate" className={styles.navLink}>Doar</a>
+                            </div>
+                        </nav>
+                    </header>
+                    <div className="container">
+                        <h1 className={styles.bannerText}>Cadastrar ONG</h1>
+                        <p className={styles.bannerText}>Preencha os campos para incluir sua ONG na plataforma.</p>
+                        <hr/>
+                        <div className="col-6">
+                            <div className="form-floating mb-3">
+                                <input type="text" id="nome" name="nome" className="form-control" value={ong.nome} onChange={onInputChange} />
+                                <label htmlFor="nome">Nome da Organização:</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" id="causa" name="causa" className="form-control" value={ong.causa} onChange={onInputChange} />
+                                <label htmlFor="causa">Causa:</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <input type="text" id="cnpj" name="cnpj" className="form-control" value={ong.cnpj} onChange={onInputChange} />
+                                <label htmlFor="cnpj">CNPJ:</label>
+                            </div>
+                        </div>
+                        <div className="col-6 mb-3">
+                            <input type="button" className="btn btn-primary col-12 p-3" value="Salvar ONG" onClick={btnSaveClick} />
+                        </div>
+                        <div className="col-6 mb-3">
+                            <Link href="/" className="btn btn-secondary col-12 p-3">Voltar</Link>
+                        </div>
+                        {
+                            message
+                                ? <div className="alert alert-success p-3 col-6" role="alert" dangerouslySetInnerHTML={{ __html: message }}></div>
+                                : <></>
+                        }
                     </div>
                 </div>
-                <div className="col-6 mb-3">
-                    <input type="button" className="btn btn-primary col-12 p-3" value="Salvar ONG" onClick={btnSaveClick} />
-                </div>
-                <div className="col-6 mb-3">
-                    <Link href="/" className="btn btn-secondary col-12 p-3">Voltar</Link>
-                </div>
-                {
-                    message
-                        ? <div className="alert alert-success p-3 col-6" role="alert" dangerouslySetInnerHTML={{ __html: message }}></div>
-                        : <></>
-                }
-                <Footer />
-            </div>
-        </>
-    );
+            </main>
+            </>
+        );
 }
--- dapp/src/pages/index.js
+++ dapp/src/pages/index.js
@@ -1,10 +1,12 @@
-import { useState } from 'react';
+import React from 'react';
 import Head from 'next/head';
+import styles from '@/styles/teste.module.css';
+import { useState } from 'react';
 import Link from 'next/link';
-import Footer from '@/components/Footer';
 import { doLogin } from '@/services/Web3Service';
 
-export default function Home() {
+
+export default function inicio() {
 
   const [wallet, setWallet] = useState("");
   const [error, setError] = useState("");
@@ -14,48 +16,41 @@ export default function Home() {
       .then(wallet => setWallet(wallet))
       .catch(err => setError(err.message));
   }
-
   return (
     <>
       <Head>
-        <title>GLUPS | Home</title>
-        <meta charSet='utf-8' />
-        <meta name="description" content="" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>Glups - Home</title>
+        <meta name="description" content="Glups" />
       </Head>
-      <div className="container px-4 py-5">
-        <div className="row flex-lg-row-reverse align-items-center py-5 g-5">
-          {
-            !wallet
-              ? (
-                <div className="col-10 col-sm-8 col-lg-6">
-                  <img src="https://cdn.discordapp.com/attachments/1220135642093195294/1220533597350658130/principal.png?ex=660f497e&is=65fcd47e&hm=a6b1a1d9be3ef97362f47c528dec36719184e4a87a0e7a258b4b7c04ac123ea2&" className="d-block mx-lg-auto img-fluid" width="700" height="500" />
-                </div>
-              )
-              : (
-                <div className="col-10 col-sm-8 col-lg-6">
-                  <p className='mb-3'>Seja bem vindo {wallet}</p>
-                  <p className='mb-3'>O que você deseja fazer?</p>
-                  <div className='col-12'>
-                    <p><Link href="/donate" className='btn btn-primary col-6 p-3'>Quero fazer uma doação</Link></p>
-                    <p><Link href="/create" className='btn btn-secondary col-6 p-3'>Quero criar uma campanha</Link></p>
-                  </div>
-                </div>
-              )
-          }
 
-          <div className="col-lg-6">
-            <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">GLUPS</h1>
-            <p className='lead'>Uma plataforma descentralizada de doações.</p>
-            <h3>1°Passo:</h3>
-            <p className='lead mb-3'>Conecte sua carteira</p>
-            <h3>2°Passo:</h3>
-            <p className='lead mb-3'>Cadastre sua ONG ou seja um doador<br></br></p>
+      <header className={styles.header}>
+        <nav className={styles.navbar}>
+          <div className={styles.logo}>Glups</div>
+          <div className={styles.navItems}>
+            <a href="/" className={styles.navLink}>Home</a>
+            <a href="/about" className={styles.navLink}>Sobre</a>
+            <a href="/create" className={styles.navLink}>Cadatrar ONG</a>
+            <a href="/donate" className={styles.navLink}>Doar</a>
+          </div>
+        </nav>
+      </header>
+
+
+      <main className={styles.main}>
+        <div className={styles.sobreposicao}>
+          <div className={styles.banner}>
+            <h1>Ajudando ONGS e doadores</h1>
+            <h4>O objetivo da nossa plataforma é a transparência</h4>
+            <h5 className={styles.h5}>
+              1° Passo: Conecte-se na MetaMask <br>
+              </br>
+              2° Passo: Receba ou faça doações
+            </h5>
             {
               !wallet
                 ? (
-                  <div className="d-grid gap-2 d-md-flex justify-content-md-start">
-                    <button type='button' className='btn btn-primary btn-lg px-4 me-md-2' onClick={btnLoginClick}>
+                  <div>
+                    <button type='button' className={styles.donateButton} onClick={btnLoginClick}>
                       <img src="/metamask.svg" width="64" className="me-3" />
                       Conectar com a MetaMask
                     </button>
@@ -66,12 +61,22 @@ export default function Home() {
                     }
                   </div>
                 )
-                : <></>
+                : (
+                  <div>
+                    <p>Seja bem vindo {wallet}</p>
+                    <p>O que você deseja fazer?</p>
+                    <button type='button' className={styles.donateB} onClick={btnLoginClick}>
+                      <a className={styles.donateButton2} href="/donate">Doar</a>
+                      <br /><br />
+                      <a className={styles.donateButton2} href="/create">Cadatrar ONG</a>
+                    </button>
+                  </div>
+                )
             }
           </div>
         </div>
-        <Footer />
-      </div>
+      </main>
     </>
-  )
-}
+  );
+};
+
